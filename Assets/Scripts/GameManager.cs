e92e8da13af3f92e42ce3981f036851c244313ae
Kirsty Adair <kirstyadair99@gmail.com>
2021-01-24 17:21:26 +0000

    Started on spawning tokens

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameObject tokenPrefab;
+
+    private int numSpawned = 0;
+    private int numToSpawn = 20;
+
+    private Entity tokenEntityPrefab;
+    private EntityManager manager;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, new BlobAssetStore());
+        tokenEntityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(tokenPrefab, settings);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numSpawned < numToSpawn)
+        {
+            manager.Instantiate(tokenEntityPrefab);
+            numSpawned++;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerData.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct SpawnerData : IComponentData
+{
+    public int numToSpawn;
+    public int numAlreadySpawned;
+    public Entity tokenToSpawn;
+}
